65050be7f0266b30cfeb1a62c44bbac9dc1a1db6
Joe-Simpson <the.fluff.lives.on@gmail.com>
2018-05-30 12:53:37 +0100

    asidj

--- app/Conversation.php
+++ app/Conversation.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-    //
+    public function messages()
+    {
+        return $this->hasMany('App\Message')->orderBy('created_at', 'asc');
+    }
+    public function user1()
+    {
+        return $this->belongsTo('App\User','user1');
+    }
+    public function user2()
+    {
+        return $this->belongsTo('App\User','user2');
+    }
 }
--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    protected $fillable = [
+        'text'
+    ];
+
+    public function conversation()
+    {
+        return $this->belongsTo('App\Conversation');
+    }
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
